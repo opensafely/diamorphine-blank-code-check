caed04e6f4a9fbc283ba2ec19b0a1b12880602e8
Jon Massey <jon.massey@thedatalab.org>
2023-10-31 14:32:04 +0000

    fix sql query

--- analysis/diamorphine.sql
+++ analysis/diamorphine.sql
@@ -2,24 +2,29 @@
     cte
     as
     (
-        SELECT 
-            dmd_id, 
-            year(ConsultationDate) as [year], 
-            month(ConsultationDate) as [month], 
+        SELECT
+            dmd_id,
+            year(ConsultationDate) as [year],
+            month(ConsultationDate) as [month],
             cast(count(*) as float) as IssueCount
         FROM MedicationIssue i
             JOIN MedicationDictionary d
             on i.MultilexDrug_ID = d.MultilexDrug_ID
         WHERE 
-            lower(d.FullName) like '%diamorph%' or 
-            lower(d.RootName) like '%diamorph%' or 
-            lower(d.FullName) like '%ayendi%' or 
+            lower(d.FullName) like '%diamorph%' or
+            lower(d.RootName) like '%diamorph%' or
+            lower(d.FullName) like '%ayendi%' or
             lower(d.rootname) like '%ayendi'
+        GROUP BY
+            dmd_id,
+            year(ConsultationDate),
+            month(ConsultationDate)
     )
 
 select
     dmd_id,
     [Year],
-    [Month], 
+    [Month],
     CASE WHEN IssueCount=0 THEN 0 ELSE (CEILING(IssueCount/6)*6) - 3 END as IssueCount_midpoint6
 from cte
+order by 1,2,3
