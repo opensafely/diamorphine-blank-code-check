58a8886f491f02ca745f7a8255cec72c8029a394
Jon Massey <jon.massey@thedatalab.org>
2023-10-31 14:06:13 +0000

    Init

---
+++ analysis/diamorphine.sql
@@ -0,0 +1,25 @@
+;with
+    cte
+    as
+    (
+        SELECT 
+            dmd_id, 
+            year(ConsultationDate) as [year], 
+            month(ConsultationDate) as [month], 
+            cast(count(*) as float) as IssueCount
+        FROM MedicationIssue i
+            JOIN MedicationDictionary d
+            on i.MultilexDrug_ID = d.MultilexDrug_ID
+        WHERE 
+            lower(d.FullName) like '%diamorph%' or 
+            lower(d.RootName) like '%diamorph%' or 
+            lower(d.FullName) like '%ayendi%' or 
+            lower(d.rootname) like '%ayendi'
+    )
+
+select
+    dmd_id,
+    [Year],
+    [Month], 
+    CASE WHEN IssueCount=0 THEN 0 ELSE (CEILING(IssueCount/6)*6) - 3 END as IssueCount_midpoint6
+from cte
